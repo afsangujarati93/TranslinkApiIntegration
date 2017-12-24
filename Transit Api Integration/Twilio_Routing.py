93f876a39299c5de57d1e003c3eb3d193fc3a41a
afsan <afsan.gujarati@gmail.com>
2017-12-24 19:13:30 -0400

    Changes made as per the github code

--- Transit Api Integration/Twilio_Routing.py
+++ Transit Api Integration/Twilio_Routing.py
@@ -102,7 +102,6 @@ def UserInputSchedule():
         resp.hangup()
         return str(resp)     
 
-
 if __name__ == "__main__":
 #    logger.debug("In routing main")
     app.run(debug=True, host='127.0.0.1', port=8080, threaded=True)
