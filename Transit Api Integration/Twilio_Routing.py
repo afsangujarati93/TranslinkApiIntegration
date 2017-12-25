613c594d49637f9c540a4500fa562554f0434dfe
afsan <afsan.gujarati@gmail.com>
2017-12-25 13:04:12 -0400

    Deploying changes for custom docker file. Lessons: when using app.yaml, use custom docker file and 0.0.0.0 ip is used to allow url to be accessible from outisde

--- Transit Api Integration/Twilio_Routing.py
+++ Transit Api Integration/Twilio_Routing.py
@@ -104,5 +104,5 @@ def UserInputSchedule():
 
 if __name__ == "__main__":
 #    logger.debug("In routing main")
-    app.run(debug=True, host='127.0.0.1', port=8080, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=8080, threaded=True)
 #	app.run(debug=True, threaded=True)
